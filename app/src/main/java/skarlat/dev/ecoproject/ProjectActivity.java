29d48948bc126be61505d898f4721f94d2a292f1
asybil <skarlat-denis@mail.ru>
2020-05-04 19:06:26 +0300

    Add intent in ProjectActivity

--- app/src/main/java/skarlat/dev/ecoproject/ProjectActivity.java
+++ app/src/main/java/skarlat/dev/ecoproject/ProjectActivity.java
@@ -2,7 +2,9 @@ package skarlat.dev.ecoproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /*
 **  Project Activity
@@ -10,10 +12,17 @@ import android.os.Bundle;
 *
  */
 public class ProjectActivity extends AppCompatActivity {
+	Bundle savedInstanceState;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_project);
+		this.savedInstanceState = savedInstanceState;
+	}
+	
+	protected void openCard(View view){
+		Intent intent = new Intent(ProjectActivity.this, EcoCardActivity.class);
+		startActivity(intent, savedInstanceState);
 	}
 }
