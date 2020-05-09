5948ac6b00482930b780f21afd51d72992c88f61
asybil <skarlat-denis@mail.ru>
2020-05-09 03:20:59 +0300

    Created custom switch button from ToggleButton, Added scrollView

--- app/src/main/java/skarlat/dev/ecoproject/EcoCardActivity.java
+++ app/src/main/java/skarlat/dev/ecoproject/EcoCardActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.app.ActionBar;
+import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -16,7 +17,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 import android.widget.Toolbar;
 
 import com.jackandphantom.customtogglebutton.CustomToggle;
@@ -28,17 +31,51 @@ import java.util.List;
 public class EcoCardActivity extends AppCompatActivity {
 	public static String TAG = "EcoCardActivity";
 	private RecyclerView recyclerView;
+	private ScrollView scrollView;
 	List<EcoSoviet> ecoSoviets = new ArrayList<>();
+	Drawable whyBackground;
+	Drawable howBackground;
+	ColorStateList whyColor;
+	ColorStateList howColor;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_eco_cards);
 		
+		final TextView how = (TextView) findViewById(R.id.how_text);
+		final TextView why = (TextView) findViewById(R.id.why_text);
+		
+		whyColor = why.getTextColors();
+		howColor = how.getTextColors();
+		
+		whyBackground = why.getBackground();
+		howBackground = how.getBackground();
+		ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleBtn);
+		toggleButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Drawable tmp = whyBackground;
+				whyBackground = howBackground;
+				howBackground = tmp;
+				
+				ColorStateList tmpColor = whyColor;
+				whyColor = howColor;
+				howColor = tmpColor;
+				
+				how.setTextColor(howColor);
+				how.setBackground(howBackground);
+				why.setTextColor(whyColor);
+				why.setBackground(whyBackground);
+				changeVisibility();
+			}
+		});
 		
 		initiList();
 		Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
 //		setSupportActionBar(myToolbar);
+		
 		setActionBar(myToolbar);
 		myToolbar.setTitle("Экономим водные ресурсы");
 		myToolbar.setSubtitle("Ресурсосбережение");
@@ -50,35 +87,14 @@ public class EcoCardActivity extends AppCompatActivity {
 		actionBar.setTitle("Экономим водные ресурсы");
 		
 		
-		
 		recyclerView = (RecyclerView) findViewById(R.id.recycle_soviets);
+		scrollView = (ScrollView) findViewById(R.id.scroll_description);
 		DataAdapter dataAdapter = new DataAdapter(EcoCardActivity.this, ecoSoviets);
 		recyclerView.setAdapter(dataAdapter);
-		
-		
-//		if (actionBar != null) {
-//			log("Action bar is: " + actionBar.toString());
-//			actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP);
-//			ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, 400);
-//			actionBar.setDisplayOptions(actionBar.DISPLAY_SHOW_CUSTOM);
-//		}
-//		else{
-//			Toolbar toolbar = new Toolbar(EcoCardActivity.this);
-//			toolbar.setTitle("hello");
-//			toolbar.setSubtitle("subtitle");
-//
-////			this.getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
-//			toolbar.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-//			toolbar.setLayoutParams(new Toolbar.LayoutParams(Toolbar.LayoutParams.MATCH_PARENT,400 ));
-//			setSupportActionBar(null);
-//			setSupportActionBar(toolbar);
-//			toolbar.setVisibility(View.VISIBLE);
-//			log("Created toolbar");
-//			log("Toolbar is visible: " + toolbar.isDrawingCacheEnabled());
-////			this.addContentView(toolbar, new ViewGroup.LayoutParams(Toolbar.LayoutParams.MATCH_PARENT,400 ));
-////			log("Toolbar is visible: " + toolbar.isDrawingCacheEnabled());
-//		}
+
 	}
+	
+	
 	 private void log(String message){
 		Log.d(TAG, message);
 	}
@@ -94,5 +110,15 @@ public class EcoCardActivity extends AppCompatActivity {
 		ecoSoviets.add(new EcoSoviet("Купить и успокоиться",
 				"Поставить на кран водо-сберегающую насадку-аэратор. " +
 						"Она стоит от 100 рублей в магазинах сантехники.", false));
+	}
+	
+	protected  void changeVisibility(){
+		if (recyclerView.getVisibility() == View.VISIBLE){
+			recyclerView.setVisibility(View.GONE);
+			scrollView.setVisibility(View.VISIBLE);
+		}else {
+			scrollView.setVisibility(View.GONE);
+			recyclerView.setVisibility(View.VISIBLE);
+		}
 	}
 }
