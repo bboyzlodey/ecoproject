7fe191938a31deab91d669bccfb7a584a6e636c0
asybil <skarlat-denis@mail.ru>
2020-05-28 00:08:35 +0300

    Add fonts, change activity and layout

--- app/src/main/java/skarlat/dev/ecoproject/activity/EcoCardActivity.java
+++ app/src/main/java/skarlat/dev/ecoproject/activity/EcoCardActivity.java
@@ -49,68 +49,12 @@ public class EcoCardActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_eco_cards);
-
 		db = new DatabaseHelper();
-
 		Bundle bundle = getIntent().getExtras();
-
 		ecoCard = (EcoCard) bundle.get(EcoCard.class.getSimpleName());
-
-		fullDescView = findViewById(R.id.full_description);
-		final TextView how = (TextView) findViewById(R.id.how_text);
-		final TextView why = (TextView) findViewById(R.id.why_text);
-		
-		whyColor = why.getTextColors();
-		howColor = how.getTextColors();
-		
-		whyBackground = why.getBackground();
-		howBackground = how.getBackground();
-		ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleBtn);
-		toggleButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Drawable tmp = whyBackground;
-				whyBackground = howBackground;
-				howBackground = tmp;
-				
-				ColorStateList tmpColor = whyColor;
-				whyColor = howColor;
-				howColor = tmpColor;
-				
-				how.setTextColor(howColor);
-				how.setBackground(howBackground);
-				why.setTextColor(whyColor);
-				why.setBackground(whyBackground);
-				changeVisibility();
-			}
-		});
-		
 		initiList();
-		setFulDesc();
-		myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-		initActionBar();
-		scrollView = (ScrollView) findViewById(R.id.scroll_description);
-		/**
-		 *
-		 *      Добавляем лист объектов в recycleView;
-		 */
-		recyclerView = (RecyclerView) findViewById(R.id.recycle_soviets);
-		DataAdapter dataAdapter = new DataAdapter(EcoCardActivity.this, ecoSoviets);
-		recyclerView.setAdapter(dataAdapter);
-
 	}
-	
-	private void initActionBar(){
-		setActionBar(myToolbar);
-		myToolbar.setTitle(ecoCard.getTitle());
-		myToolbar.setSubtitle(ecoCard.getDescription());
-		ActionBar actionBar = this.getActionBar();
-		actionBar.setDisplayHomeAsUpEnabled(true);
-		actionBar.setBackgroundDrawable(getDrawable(R.drawable.original));
-		actionBar.setTitle(ecoCard.getTitle());
-		actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back2);
-	}
-	
+		
 	
 	 private void log(String message){
 		Log.d(TAG, message);
@@ -119,35 +63,8 @@ public class EcoCardActivity extends AppCompatActivity {
 	protected void initiList(){
 		ecoSoviets = db.getAllByCardName(ecoCard.getName());
 	}
-
-	private void setFulDesc(){
-		LinearLayout layout = findViewById(R.id.full_description);
-		Resources res = getResources();
-		String[] text = res.getStringArray(R.array.full_desc);
-		TypedArray icons = res.obtainTypedArray(R.array.img);
-
-
-		TextView textView = new TextView(this);
-		textView.setText(text[0]);
-
-		Drawable drawable = icons.getDrawable(0);
-		ImageView imageView = new ImageView(this);
-		imageView.setBackground(drawable);
-		layout.addView(textView);
-		layout.addView(imageView);
-
-	}
 	
-	protected  void changeVisibility(){
-		if (recyclerView.getVisibility() == View.VISIBLE){
-			recyclerView.setVisibility(View.GONE);
-			scrollView.setVisibility(View.VISIBLE);
-		}else {
-			scrollView.setVisibility(View.GONE);
-			recyclerView.setVisibility(View.VISIBLE);
-		}
-	}
-
+	
 	public void onBackBtn(View view){
 		onBackPressed();
 	}
@@ -158,17 +75,4 @@ public class EcoCardActivity extends AppCompatActivity {
 		setResult(RESULT_OK);
 		finish();
 	}
-
-	public void likeBtn(View view){
-		EcoSoviet tip = (EcoSoviet) view.getTag();
-		if (tip.getStatus() == EcoSoviet.Status.UNLIKED) {
-			tip.upDate(EcoSoviet.Status.LIKED);
-			db.updateFirebaseProgress("Tips" ,String.valueOf(tip.sovietID), "status", 1);
-		}
-		else {
-			tip.upDate(EcoSoviet.Status.UNLIKED);
-			db.updateFirebaseProgress("Tips" ,String.valueOf(tip.sovietID), "status", 0);
-		}
-	}
-
 }
