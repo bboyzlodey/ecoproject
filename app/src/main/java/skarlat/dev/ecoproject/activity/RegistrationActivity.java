ff19743633940586a2a7193688e4590a36ca3273
asybil <skarlat-denis@mail.ru>
2020-07-05 16:28:44 +0300

    Created RegistrationActivity

---
+++ app/src/main/java/skarlat/dev/ecoproject/activity/RegistrationActivity.java
@@ -0,0 +1,44 @@
+package skarlat.dev.ecoproject.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import skarlat.dev.ecoproject.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+	private TextInputEditText nameEditText;
+	private TextInputEditText emailEditText;
+	private TextInputEditText passwdEditText;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_registration);
+		nameEditText = (TextInputEditText) findViewById(R.id.name);
+		emailEditText = (TextInputEditText) findViewById(R.id.user_email);
+		passwdEditText = (TextInputEditText) findViewById(R.id.user_passwd);
+		
+	}
+	
+	/**
+	 * @TODO: Write code for:
+	 *          1. sign_in_google
+	 *          2. sign_in
+	 *          3. submit
+	 */
+	public void onClick(View v){
+		int id = v.getId();
+		switch (id){
+			case R.id.sign_in_google:
+				break;
+			case R.id.sign_in:
+				break;
+			case R.id.submit:
+				break;
+		}
+	}
+}
