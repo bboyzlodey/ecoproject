434c246c6059f58ff2eed4ac46078ee0f8208a51
asybil <skarlat-denis@mail.ru>
2020-07-17 19:59:54 +0300

    Create TabLayout for tests

--- app/src/main/java/skarlat/dev/ecoproject/TestActivity.java
+++ app/src/main/java/skarlat/dev/ecoproject/TestActivity.java
@@ -17,16 +17,16 @@ public class TestActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test);
-		FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
-				getSupportFragmentManager(), FragmentPagerItems.with(this)
-						                             .add("2", PageFragment.class)
-						                             .add("1".concat(" !"), PageFragment.class)
-						                             .create());
-		
-		ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-		viewPager.setAdapter(adapter);
-		
-		SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
-		viewPagerTab.setViewPager(viewPager);
+//		FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+//				getSupportFragmentManager(), FragmentPagerItems.with(this)
+//						                             .add("2", PageFragment.class)
+//						                             .add("1".concat(" !"), PageFragment.class)
+//						                             .create());
+//
+//		ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+//		viewPager.setAdapter(adapter);
+//
+//		SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+//		viewPagerTab.setViewPager(viewPager);
 	}
 }
