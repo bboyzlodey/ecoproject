bff38a3ebf0c1cac3b5f8703320a0ac162ba910d
Denis Skarlat <denis.skarlat@quantron-systems.com>
2020-11-02 23:03:57 +0300

    Update data

--- app/src/main/java/skarlat/dev/ecoproject/ExtendedFunctions.kt
+++ app/src/main/java/skarlat/dev/ecoproject/ExtendedFunctions.kt
@@ -2,6 +2,7 @@ package skarlat.dev.ecoproject
 
 import android.content.Context
 import android.util.TypedValue
+import android.view.View
 
 fun Int.pxToDp(context: Context): Int = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), context.resources.displayMetrics
@@ -10,3 +11,11 @@ fun Int.pxToDp(context: Context): Int = TypedValue.applyDimension(
 fun Int.dpToPx(context: Context): Int = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_PX, this.toFloat(), context.resources.displayMetrics
 ).toInt()
+
+fun View.show(show: Boolean = false) {
+    visibility = if (show) {
+        View.VISIBLE
+    } else {
+        View.GONE
+    }
+}
