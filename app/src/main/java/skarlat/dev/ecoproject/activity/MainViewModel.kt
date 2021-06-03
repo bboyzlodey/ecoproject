b5db2f3bc1a8647957d48508a3fe4e0c80bbc323
Denis Skarlat <denis.skarlat@quantron-systems.com>
2021-06-04 02:40:36 +0300

    Add other: viewModels, drawables, SingleLiveData (need tested)

---
+++ app/src/main/java/skarlat/dev/ecoproject/activity/MainViewModel.kt
@@ -0,0 +1,9 @@
+package skarlat.dev.ecoproject.activity
+
+import androidx.lifecycle.ViewModel
+import skarlat.dev.ecoproject.core.AppCache
+import javax.inject.Inject
+
+class MainViewModel @Inject constructor(private val appCache: AppCache): ViewModel() {
+
+}
