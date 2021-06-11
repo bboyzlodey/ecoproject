2a868cb2bb0797463b27ff980fe784fd6dd11c00
Denis Skarlat <denis.skarlat@quantron-systems.com>
2021-06-12 00:05:38 +0300

    Handle navigation to user fragment

--- app/src/main/java/skarlat/dev/ecoproject/fragment/HomeFragment.kt
+++ app/src/main/java/skarlat/dev/ecoproject/fragment/HomeFragment.kt
@@ -27,6 +27,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.profileImage.setOnClickListener { viewModel.onProfileClicked() }
     }
 
     override fun onStart() {
--- app/src/main/java/skarlat/dev/ecoproject/fragment/HomeViewNodel.kt
+++ app/src/main/java/skarlat/dev/ecoproject/fragment/HomeViewNodel.kt
@@ -1,14 +1,12 @@
 package skarlat.dev.ecoproject.fragment
 
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import skarlat.dev.ecoproject.EcoTipsApp
-import skarlat.dev.ecoproject.User
-import skarlat.dev.ecoproject.di.DaggerAppComponent
 
 class HomeViewModel: BaseViewModel() {
+
+    fun onProfileClicked() {
+        nextScreen.tryEmit(HomeFragmentDirections.actionHomeFragmentToUserFragment())
+    }
 
     private val appCache = EcoTipsApp.appComponent.getAppCache()
     val user get() = appCache.userFlow
